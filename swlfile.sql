f33f4ae8715b909d9e88c01c831abde9997de1e6
rajashekarg12 <110624878+rajashekarg12@users.noreply.github.com>
2026-02-27 09:01:37 +0530

    Update swlfile.sql

--- swlfile.sql
+++ swlfile.sql
@@ -1,16 +1,14 @@
 SET SERVEROUTPUT ON
 
-CREATE OR REPLACE PROCEDURE test_deploy_proc
-AS
-    EXIT_ERROR            EXCEPTION;
-    EXIT_NOERROR          EXCEPTION;
-    eExit                 EXCEPTION;
-    vGeneralException     EXCEPTION;
+CREATE OR REPLACE PROCEDURE test_deploy_proc AS
+    EXIT_ERROR        EXCEPTION;
+    EXIT_NOERROR      EXCEPTION;
+    eExit             EXCEPTION;
+    vGeneralException EXCEPTION;
 BEGIN
-    -- Simulate long/hanging deployment
-    DBMS_LOCK.SLEEP(600);  -- 10 minutes
+    NULL;
 END;
 /
 
--- Execute it (this is what causes the hang)
-EXEC test_deploy_proc;
+-- This will cause SQL*Plus to wait for input
+SELECT * FROM dual WHERE dummy='&ENTER_VALUE';
