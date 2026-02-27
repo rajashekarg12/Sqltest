204c6403dc295afbbb579548fb01a346509e0b1c
rajashekarg12 <110624878+rajashekarg12@users.noreply.github.com>
2026-02-27 07:26:22 +0530

    Update swlfile.sql

--- swlfile.sql
+++ swlfile.sql
@@ -1,8 +1,22 @@
 SET SERVEROUTPUT ON
 
-DECLARE
+CREATE OR REPLACE PROCEDURE test_deploy_proc
+AS
     EXIT_ERROR            EXCEPTION;
     EXIT_NOERROR          EXCEPTION;
     eExit                 EXCEPTION;
     vGeneralException     EXCEPTION;
 
+    lErrMsg VARCHAR2(2000);
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Start procedure');
+
+    -- Intentional compilation error
+    invalid_variable := 10;
+
+EXCEPTION
+    WHEN EXIT_ERROR THEN
+        lErrMsg := 'Error encountered';
+        DBMS_OUTPUT.PUT_LINE(lErrMsg);
+END;
+/
